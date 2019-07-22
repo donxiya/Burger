23bbeaea6adb33193a5616603dbf118b5e0eb89b
donxiya <31514669+donxiya@users.noreply.github.com>
2019-07-22 17:01:01 -0500

    git ignore

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,40 +1,33 @@
 const express = require("express");
-var router = express.app(); 
+var router = express.app();
 const burger = require("../models/burger_model.js");
 
-router.get("/", function(req, res) {
-    console.log("Route Hit");
-    burger.selectAll((data) => {
-      handlebarsObject = {
-        burger: data
-      };
-      console.log("Diplayed Burgers");
-      res.render("index", handlebarsObject);
-    });
-    
+router.get("/", function (req, res) {
+  burger.selectAll((data) => {
+    handlebarsObject = {
+      burger: data
+    };
+    res.render("index", handlebarsObject);
+  });
+
 });
 
 //Post routes
 
-router.post("/api/burger", function(req, res) {
-  console.log("Route Hit");
-  burger.insertOne(["burger_name","devoured"], [req.body["burger_name"], req.body.devoured], (result)=>{
-    // Send back the ID of the new quote
-    console.log(result);
+router.post("/api/burger", function (req, res) {
+  burger.insertOne(["burger_name", "devoured"], [req.body["burger_name"], req.body.devoured], (result) => {
     res.json(result);
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/api/burger/:id", function (req, res) {
 
   let burgerID = req.params.id
-  let condition = "id = " + burgerID ;
+  let condition = "id = " + burgerID;
 
-  console.log("Route Hit. ID "+ burgerID);
-  console.log("Devour is " + req.body.devoured);
 
-  burger.updateOne(["devoured"], [req.body.devoured], condition, (result)=>{
-    console.log("1st callBack");
+
+  burger.updateOne(["devoured"], [req.body.devoured], condition, (result) => {
     res.json(result);
   });
 });
